57a57e26eeadca34fda8c2060f013128c6297bc5
a22sebba <a22sebba@student.his.se>
2026-03-26 18:49:53 +0100

    added enemy rnd stats from Program.cs

--- OBP200-RolePlayingGame/Enemy.cs
+++ OBP200-RolePlayingGame/Enemy.cs
@@ -2,5 +2,37 @@
 
 public class Enemy
 {
+    static List<string[]> EnemyTemplates = new List<string[]>();
     
+    
+    static string[] GenerateEnemy(bool isBoss)
+    {
+        if (isBoss)
+        {
+            // Boss-mall
+            return new[] { "boss", "Urdraken", "55", "9", "4", "30", "50" };
+        }
+        else
+        {
+            // Slumpa bland templates
+            var template = EnemyTemplates[Rng.Next(EnemyTemplates.Count)];
+            
+            // Slmumpmässig justering av stats
+            int hp = ParseInt(template[2], 10) + Rng.Next(-1, 3);
+            int atk = ParseInt(template[3], 3) + Rng.Next(0, 2);
+            int def = ParseInt(template[4], 0) + Rng.Next(0, 2);
+            int xp = ParseInt(template[5], 4) + Rng.Next(0, 3);
+            int gold = ParseInt(template[6], 2) + Rng.Next(0, 3);
+            return new[] { template[0], template[1], hp.ToString(), atk.ToString(), def.ToString(), xp.ToString(), gold.ToString() };
+        }
+    }
+    
+    static void InitEnemyTemplates()
+    {
+        EnemyTemplates.Clear();
+        EnemyTemplates.Add(new[] { "beast", "Vildsvin", "18", "4", "1", "6", "4" });
+        EnemyTemplates.Add(new[] { "undead", "Skelett", "20", "5", "2", "7", "5" });
+        EnemyTemplates.Add(new[] { "bandit", "Bandit", "16", "6", "1", "8", "6" });
+        EnemyTemplates.Add(new[] { "slime", "Geléslem", "14", "3", "0", "5", "3" });
+    }
 }
